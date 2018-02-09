07ae88b83d2cab1c19599c204a49d18ecb7dd3cc
FoxChrisRealTheGit <cfox1290@gmail.com>
2018-02-08 20:20:51 -0700

    nav almost works

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import 'aframe';
 import 'aframe-particle-system-component';
 import { Scene } from 'aframe-react';
-import Text from './components/blocks0.0.1'
+import Text, {Navbar1} from './components/blocks0.0.1'
 import Container1 from './components/containers0.0.1';
 class App extends Component {
   constructor(){
@@ -13,9 +13,10 @@ class App extends Component {
   render() {
     const stuff =<Text childs={["Hello World!", "Or not"]} position="0 2 -15" />
     const More = <Text childs={["Hello World!", "Or not"]} position="2 2 -15" />
+    const nav = <Navbar1 childs={['/', '/', '/']} position="-15 2 0" rotation="0 90 0" move="0 2 0" />
     return (
       <Container1
-      childs={[stuff,More]} />
+      childs={[stuff,More, nav]} />
 
     );
   }
--- src/components/blocks0.0.1.js
+++ src/components/blocks0.0.1.js
@@ -1,25 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import 'aframe';
 import 'aframe-particle-system-component';
-import {Entity} from 'aframe-react';
+import { Entity } from 'aframe-react';
 
-export default class Text extends Component{
-    constructor(props){
+export default class Text extends Component {
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             placement: props.position || '0 2 -5',
             color: props.color || 'black',
-            childs:props.childs,
+            childs: props.childs,
         }
     }
-    ComponentWillMount(){
-        const CHILDS =  React.Children.toArray(this.props.children)
+    ComponentWillMount() {
+        const CHILDS = React.Children.toArray(this.props.children)
         this.setState({ childs: CHILDS })
     }
 
-    render(){
-        return(
-        <a-text color={this.state.color} side='double' value={this.state.childs[0]} position={this.state.placement}></a-text>
+    render() {
+        return (
+            <a-text color={this.state.color} side='double' value={this.state.childs[0]} position={this.state.placement}></a-text>
+        )
+    }
+}
+
+export class Navbar1 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            childs: props.childs,
+            position: props.position,
+            rotate: props.rotation,
+            move: props.move,
+        }
+    }
+    render() {
+        const nav = this.state.childs.map((x, i, arr)=>{
+            return  <a-link href={this.state.childs[i]} position={this.state.position + i} rotation={this.state.rotate}></a-link>
+        })
+        return (
+               nav
         )
     }
 }
